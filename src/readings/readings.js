44f89523c4272aead508611f14bdc928dcc9986e
unintendedbear <unintendedbear@gmail.com>
2021-08-19 17:22:11 +0200

    Adding implementation to take the readings from the last 7 days

--- src/readings/readings.js
+++ src/readings/readings.js
@@ -4,7 +4,14 @@ const readings = (data) => ({
         const currentReadings = data[meterId];
         data[meterId] = [...currentReadings, ...readings];
         return data[meterId];
-    },
+    }
 });
 
-module.exports = { readings };
+const getLastWeekReadings = (readings) => {
+    const now = Math.floor(Date.now() / 1000),
+        sevenDaysInSeconds = 7 * 24 * 60 * 60,
+        lastWeek = now - sevenDaysInSeconds;
+    return readings.filter((reading) => reading.time >= lastWeek);
+}
+
+module.exports = { getLastWeekReadings, readings };
